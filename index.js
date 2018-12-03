cba23ed94c9a8874728e7b55068003ba97d70d86
Nicolas Udy <dev@udy.io>
2018-12-03 17:46:12 -0500

    Prep for first major

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const rules = require('./rules')
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss'],
   plugins: [
-    'stylelint-order',
+    '@udyux/stylelint-order',
     'stylelint-declaration-strict-value',
     'stylelint-declaration-block-no-ignored-properties'
   ],
--- rules/rulesOrder.js
+++ rules/rulesOrder.js
@@ -1,35 +1,25 @@
-const selectorRules = {
-  isKeyframesStart: { type: 'rule', selector: /^(from|0%)$/ },
-  isKeyframesStep: { type: 'rule', selector: /^(\d\d?%)$/ },
-  isKeyframesEnd: { type: 'rule', selector: /^(to|100%)$/ },
-  isPseudoContent: { type: 'rule', selector: /^&?::[\w-]+$/ },
-  isChild: { type: 'rule', selector: /^[^&~+>]+$/ },
-  isSiblingOrChildOf: { type: 'rule', selector: /^[\w.&][^[_](.|\n)+&?$/ },
-  isTwin: { type: 'rule', selector: /^&[\s~+]+&$/ },
-  hasModifier: { type: 'rule', selector: /^\.-(.|\n)+[^&]$/ },
-  hasAttribute: { type: 'rule', selector: /^&\[(.|\n)+$/ },
-  hasState: { type: 'rule', selector: /^&:(.|\n)+[^&]$/ },
-  isBemElement: { type: 'rule', selector: /^&__(.|\n)+$/ }
-}
-
 module.exports = [
   'dollar-variables',
-  { type: 'at-rule', name: 'extend', hasBlock: false },
-  { type: 'at-rule', name: 'include', parameter: 'type', hasBlock: false },
-  { type: 'at-rule', name: 'include' },
+  { hasBlock: false, name: 'extend', type: 'at-rule' },
+  { hasBlock: false, name: 'include', parameter: 'type', type: 'at-rule' },
+  { name: 'include', type: 'at-rule' },
   'declarations',
-  { type: 'at-rule', hasBlock: false },
-  { type: 'at-rule', name: 'media' },
-  { type: 'at-rule', hasBlock: true },
-  selectorRules.isKeyframesStart,
-  selectorRules.isKeyframesStep,
-  selectorRules.isKeyframesEnd,
-  selectorRules.isPseudoContent,
-  selectorRules.isChild,
-  selectorRules.isSiblingOrChildOf,
-  selectorRules.isTwin,
-  selectorRules.hasModifier,
-  selectorRules.hasAttribute,
-  selectorRules.hasState,
-  selectorRules.isBemElement
+  { hasBlock: false, type: 'at-rule' },
+  { name: 'media', type: 'at-rule' },
+  { hasBlock: true, type: 'at-rule' },
+  { label: 'pseudo-content ("&::before/&::after")', selector: /^&?::[\w-]+$/, type: 'rule' },
+  { label: 'a pseudo-class ("&:pseudo")', selector: /^&:(.|\n)+[^&]$/, type: 'rule' },
+  { label: 'an attribute ("&[attr]")', selector: /^&\[(.|\n)+$/, type: 'rule' },
+  { label: 'a BEM modifier ("&.-mod")', selector: /^&\.-\w+[^&]$/, type: 'rule' },
+  { label: 'a twin selector ("& +/~ &")', selector: /^&[\s~+]+&$/, type: 'rule' },
+  { label: 'a child of (".parent &")', selector: /^[^&~+>]+$/, type: 'rule' },
+  {
+    label: 'a nested/sibling of parent (".class >/+/~ &")',
+    selector: /^[\w.&][^[_][^+~](.|\n)+&$/,
+    type: 'rule'
+  },
+  { label: 'a BEM element ("&__elem")', selector: /^&__(.|\n)+$/, type: 'rule' },
+  { label: 'the first keyframe of an animation ("from/0%")', selector: /^(from|0%)$/, type: 'rule' },
+  { label: 'an animation keyframe ("[1-99]%")', selector: /^(\d\d?%)$/, type: 'rule' },
+  { label: 'the last keyframe of an animation ("to/100%")', selector: /^(to|100%)$/, type: 'rule' }
 ]
