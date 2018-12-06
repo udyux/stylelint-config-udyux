393c3f8076376e0c617dc9852473225ac22ddef6
Nicolas Udy <dev@udy.io>
2018-12-06 18:51:18 -0500

    Add prettier config

---
+++ prettier.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['stylelint-config-prettier'],
+  rules: {
+    'declaration-colon-newline-after': 'always-multi-line',
+    indentation: 2,
+    'value-list-comma-newline-after': 'always-multi-line'
+  }
+}
