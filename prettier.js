e2cd72922d6b49fc9e6a13fc49acb61ffb518c52
Nicolas Udy <dev@udy.io>
2018-12-06 19:29:51 -0500

    Update rule order in object due to prettier bug

--- prettier.js
+++ prettier.js
@@ -1,8 +1,8 @@
 module.exports = {
-  extends: ['stylelint-config-prettier'],
+  extends: ['./index.js', 'stylelint-config-prettier'],
   rules: {
     'declaration-colon-newline-after': 'always-multi-line',
-    indentation: 2,
-    'value-list-comma-newline-after': 'always-multi-line'
+    'value-list-comma-newline-after': 'always-multi-line',
+    indentation: 2
   }
 }
