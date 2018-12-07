cbc588cbc6fe6de912edd88f07b19e94c5331702
Nicolas Udy <dev@udy.io>
2018-12-07 11:44:27 -0500

    Update prettier config

--- prettier.js
+++ prettier.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['./index.js', 'stylelint-config-prettier'],
   rules: {
     'at-rule-name-space-after': 'always-single-line',
+    'color-hex-case': 'upper',
     'declaration-colon-newline-after': 'always-multi-line',
     'value-list-comma-newline-after': 'always-multi-line',
     indentation: 2
