1e042e603040e55cba959ccb9662f1e075470ec4
Nicolas Udy <dev@udy.io>
2019-04-10 19:37:55 -0400

    Add order rule placing pseudo-selectors before modifiers

--- rules/rulesOrder.js
+++ rules/rulesOrder.js
@@ -9,10 +9,11 @@ module.exports = [
   { hasBlock: true, type: 'at-rule' },
   { label: 'pseudo-content ("&::before/&::after")', selector: /^&?::(.|\n)+$/, type: 'rule' },
   { label: 'a twin selector ("& +/~ &")', selector: /^&\s?[~+]\s?&$/, type: 'rule' },
+  { label: 'a state/pseudo selector (&:hover/&:last-child)', selector: /^&:(.|\n)+$/, type: 'rule' },
   { label: 'a BEM modifier or var content ("&.-mod/.--")', selector: /^(&\.-|\.--)(.|\n)+$/, type: 'rule' },
-  { label: 'a pseudo/attr (&:pseudo/&[attr]) or sibling/child selector', selector: /^(?!&__)(.|\n)+$/, type: 'rule' },
+  { label: 'an attribute (&[attr]) or child/sibling (> .cls/+ .cls)', selector: /^(?!&__)(.|\n)+$/, type: 'rule' },
   { label: 'a BEM element ("&__elem")', selector: /^(&__(.|\n)+(,\n)?)+$/, type: 'rule' },
   { label: 'the first keyframe of an animation ("from/0%")', selector: /^(from|0%)$/, type: 'rule' },
   { label: 'an animation keyframe ("[1-99]%")', selector: /^(\d\d?%)$/, type: 'rule' },
   { label: 'the last keyframe of an animation ("to/100%")', selector: /^(to|100%)$/, type: 'rule' }
-]
+];
