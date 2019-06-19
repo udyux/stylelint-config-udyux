b0f22ead271579ed01f99825288a3e64975095f0
Nicolas Udy <dev@udy.io>
2019-06-19 17:17:44 -0400

    Fix non block selector

--- rules/rulesOrder.js
+++ rules/rulesOrder.js
@@ -7,9 +7,9 @@ module.exports = [
   { name: 'media', type: 'at-rule' },
   { hasBlock: true, type: 'at-rule' },
   { label: 'pseudo-content ("&::before/&::after")', selector: /^&?::(.|\n)+$/, type: 'rule' },
-  { label: 'a state or relative selector (&:hover, & + &, etc)', selector: /^(?!&__|from|to|\d+%).+$/, type: 'rule' },
+  { label: 'a modifier, state or relative', selector: /^(&:|&\.|&-|&\s|[a-z[.:])/, type: 'rule' },
   { label: 'a BEM element ("&__elem")', selector: /^(&__(.|\n)+(,\n)?)+$/, type: 'rule' },
-  { label: 'the first keyframe of an animation ("from/0%")', selector: /^(from|0%)$/, type: 'rule' },
-  { label: 'an animation keyframe ("[1-99]%")', selector: /^(\d\d?%)$/, type: 'rule' },
-  { label: 'the last keyframe of an animation ("to/100%")', selector: /^(to|100%)$/, type: 'rule' }
+  { label: 'the first keyframe of an animation', selector: /^(from|0%)$/, type: 'rule' },
+  { label: 'an animation keyframe', selector: /^(\d\d?%)$/, type: 'rule' },
+  { label: 'the last keyframe of an animation ', selector: /^(to|100%)$/, type: 'rule' }
 ]
