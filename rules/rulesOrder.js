82a1de343634eea990f0252da7ff5b687ffa8a8f
Nicolas Udy <dev@udy.io>
2022-08-10 16:31:06 -0400

    Add custom-properties to order

--- rules/rulesOrder.js
+++ rules/rulesOrder.js
@@ -1,5 +1,6 @@
 module.exports = [
   'dollar-variables',
+  'custom-properties',
   { name: 'extend', type: 'at-rule' },
   { hasBlock: false, type: 'at-rule' },
   'declarations',
@@ -10,5 +11,5 @@ module.exports = [
   { label: 'a BEM element ("&__elem")', selector: /^(&__(.|\n)+(,\n)?)+$/, type: 'rule' },
   { label: 'the first keyframe of an animation', selector: /^(from|0%)$/, type: 'rule' },
   { label: 'an animation keyframe', selector: /^(\d\d?%)$/, type: 'rule' },
-  { label: 'the last keyframe of an animation ', selector: /^(to|100%)$/, type: 'rule' }
+  { label: 'the last keyframe of an animation ', selector: /^(to|100%)$/, type: 'rule' },
 ]
