bfa6545f468b372c71f87e09e612a2f7fdb602cf
Nicolas Udy <dev@udy.io>
2022-11-16 13:50:44 -0500

    change order

--- rules/rulesOrder.js
+++ rules/rulesOrder.js
@@ -1,15 +1,18 @@
 module.exports = [
-  'dollar-variables',
-  'custom-properties',
-  { name: 'extend', type: 'at-rule' },
-  { hasBlock: false, type: 'at-rule' },
-  'declarations',
-  { name: 'media', type: 'at-rule' },
-  { hasBlock: true, type: 'at-rule' },
-  { label: 'pseudo-content ("&::before/&::after")', selector: /^&?::(.|\n)+$/, type: 'rule' },
-  { label: 'a modifier, state or relative', selector: /^(&:|&\.|&-|&\s|[a-z[.:])/, type: 'rule' },
-  { label: 'a BEM element ("&__elem")', selector: /^(&__(.|\n)+(,\n)?)+$/, type: 'rule' },
-  { label: 'the first keyframe of an animation', selector: /^(from|0%)$/, type: 'rule' },
-  { label: 'an animation keyframe', selector: /^(\d\d?%)$/, type: 'rule' },
-  { label: 'the last keyframe of an animation ', selector: /^(to|100%)$/, type: 'rule' },
+  [
+    'dollar-variables',
+    'custom-properties',
+    { name: 'extend', type: 'at-rule' },
+    { hasBlock: false, type: 'at-rule' },
+    'declarations',
+    { name: 'media', type: 'at-rule' },
+    { hasBlock: true, type: 'at-rule' },
+    { label: 'pseudo-content ("&::before/&::after")', selector: /^&?::(.|\n)+$/, type: 'rule' },
+    { label: 'a modifier or state', selector: /^(&:|&\.|&--)[^\s]+$/, type: 'rule' },
+    { label: 'a BEM element ("&__elem") or child', selector: /^(&__(.|\n)+(,\n)?|&\s|[a-z[.:])+$/, type: 'rule' },
+    { label: 'the first keyframe of an animation', selector: /^(from|0%)$/, type: 'rule' },
+    { label: 'an animation keyframe', selector: /^(\d\d?%)$/, type: 'rule' },
+    { label: 'the last keyframe of an animation ', selector: /^(to|100%)$/, type: 'rule' }
+  ],
+  { unspecified: 'bottom' }
 ]
