571191f7619244c04b07b728bf316904162abcd5
Nicolas Udy <dev@udy.io>
2023-02-22 16:18:04 -0500

    fix: allow nth- selectors in modifier regex

--- rules/rulesOrder.js
+++ rules/rulesOrder.js
@@ -7,7 +7,11 @@ module.exports = [
   { name: 'media', type: 'at-rule' },
   { hasBlock: true, type: 'at-rule' },
   { name: 'pseudo-content ("&::before/&::after")', selector: /^&?::(.|\n)+$/, type: 'rule' },
-  { name: 'modifier or state', selector: /^((&:|&\.|&--)[^\s]+(,\n?\s*)?)+$/, type: 'rule' },
+  {
+    name: 'modifier or state',
+    selector: /^((&:|&\.|&--)[^\s]+(nth-(last-)?(child|of-type)(\([n+-\d\s]+\))?)?(,\n?\s*)?)+$/,
+    type: 'rule'
+  },
   { name: 'sibling relation selector', selector: /^&\s?[+~]\s?&$/, type: 'rule' },
   { name: 'BEM element ("&__elem") or child', selector: /^(&__(.|\n)+(,\n)?|&\s|[a-z[.:])+$/, type: 'rule' },
   { name: 'first keyframe of an animation', selector: /^(from|0%)$/, type: 'rule' },
